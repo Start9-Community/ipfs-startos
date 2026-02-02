361d46b42a5a6ad6709a13ae13e847c96ae3979e
Matt Hill <mattnine@protonmail.com>
2026-02-02 11:41:26 -0700

    bump version

--- startos/install/versions/index.ts
+++ startos/install/versions/index.ts
@@ -1,2 +1,2 @@
-export { v_0_39_0_0_a0 as current } from './v0.39.0.0.a0'
+export { v_0_39_0_0_b0 as current } from './v0.39.0.0.b0'
 export const other = []
--- startos/install/versions/v0.39.0.0.b0.ts
+++ startos/install/versions/v0.39.0.0.b0.ts
@@ -1,8 +1,8 @@
 import { VersionInfo } from '@start9labs/start-sdk'
 import { rm } from 'fs/promises'
 
-export const v_0_39_0_0_a0 = VersionInfo.of({
-  version: '0.39.0:0-alpha.0',
+export const v_0_39_0_0_b0 = VersionInfo.of({
+  version: '0.39.0:0-beta.0',
   releaseNotes: {
     en_US: 'Revamped for StartOS 0.4.0',
     es_ES: 'Renovado para StartOS 0.4.0',
